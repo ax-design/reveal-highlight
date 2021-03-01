d5d9ac267b2b68045861d24adb90f346c399e431
Losses <losses.don@gmail.com>
2021-03-01 19:20:56 +0800

    feat: Support hex color

--- src/CanvasConfig.ts
+++ src/CanvasConfig.ts
@@ -11,46 +11,58 @@ const isValidateBorderDecorationType = (x: string): x is BorderDecoration => {
 };
 
 const extractRGBValue = (x: string) => {
-    let result = '';
-    let beginSearch = false;
-    let numberPointer = 0;
+    if (x[0] === '#') {
+        // Thanks bro:
+        // https://stackoverflow.com/a/11508164/3931936
+        const hexVal = parseInt(x.substring(1), 16);
 
-    for (let i = 0; i < x.length; i++) {
-        const char = x[i];
-        const charCode = char.charCodeAt(0) - 48;
-        const charIsNumber = charCode >= 0 && charCode < 10;
+        var r = (hexVal >> 16) & 255;
+        var g = (hexVal >> 8) & 255;
+        var b = hexVal & 255;
+    
+        return r + "," + g + "," + b;
+    } else {
+        let result = '';
+        let beginSearch = false;
+        let numberPointer = 0;
 
-        if (char === ' ') {
-            continue;
+        for (let i = 0; i < x.length; i++) {
+            const char = x[i];
+            const charCode = char.charCodeAt(0) - 48;
+            const charIsNumber = charCode >= 0 && charCode < 10;
+
+            if (char === ' ') {
+                continue;
+            }
+
+            if (beginSearch && !charIsNumber && char !== ',' && char !== ')') {
+                throw new SyntaxError(`${x} is not a validate color value!`);
+            }
+
+            if (!beginSearch && char === '(') {
+                beginSearch = true;
+                continue;
+            }
+
+            if (numberPointer < 2 && char === ')') {
+                throw new SyntaxError(`${x} should have at least three color channel`);
+            }
+
+            if (char === ',') {
+                numberPointer++;
+            }
+
+            if (char === ')') {
+                return result;
+            }
+
+            if (beginSearch) {
+                result += char;
+            }
         }
 
-        if (beginSearch && !charIsNumber && char !== ',' && char !== ')') {
-            throw new SyntaxError(`${x} is not a validate color value!`);
-        }
-
-        if (!beginSearch && char === '(') {
-            beginSearch = true;
-            continue;
-        }
-
-        if (numberPointer < 2 && char === ')') {
-            throw new SyntaxError(`${x} should have at least three color channel`);
-        }
-
-        if (char === ',') {
-            numberPointer++;
-        }
-
-        if (char === ')') {
-            return result;
-        }
-
-        if (beginSearch) {
-            result += char;
-        }
+        return result;
     }
-
-    return result;
 };
 
 export interface CachedBoundingRect {
