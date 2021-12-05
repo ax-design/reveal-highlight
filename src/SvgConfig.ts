aee4f921f27cd22f85023fa33115e3b76a5584e6
LOSSES Don <1384036+Losses@users.noreply.github.com>
2021-12-05 20:44:13 +0800

    feat(ng): Implement round corner

--- src/SvgConfig.ts
+++ src/SvgConfig.ts
@@ -72,6 +72,66 @@ export class SvgConfig extends BaseConfig<SVGElement> {
     };
 
     private drawShape = (hollow: boolean) => {
+        const b = this.cachedBoundingRect;
+        const w = b.width;
+        const h = b.height;
+
+        const c = this.cachedStyle;
+        const bw = c.borderWidth;
+
+        const tl = c.topLeftBorderDecorationRadius;
+        const tr = c.topRightBorderDecorationRadius;
+        const bl = c.bottomLeftBorderDecorationRadius;
+        const br = c.bottomRightBorderDecorationRadius;
+
+        const wlf = c.withLeftBorderFactor;
+        const wrf = c.withRightBorderFactor;
+        const wtf = c.withTopBorderFactor;
+        const wbf = c.withBottomBorderFactor;
+
+        const tl2 = bw < tl ? tl - bw : 0;
+        const tr2 = bw < tr ? tr - bw : 0;
+        const bl2 = bw < bl ? bl - bw : 0;
+        const br2 = bw < br ? br - bw : 0;
+
+        let d = '';
+
+
+        switch (c.borderDecorationType) {
+            // Oh... Fuck again...
+            case 'round':
+                // Step 1: Starting point
+                d += hollow ? `M ${tl * wlf}, 0 ` : `M ${bw}, ${tl2 * wtf + bw} `;
+                // Step 2: Draw shape
+                if (hollow) {
+                    // Step 2-1: This is outer path, drawing clockwise
+                    d += `
+                        h ${w - tl - tr}
+                        a ${tr}, ${tr} 0 0 1 ${tr}, ${tr} 
+                        v ${h - tr - br} 
+                        a ${br}, ${br} 0 0 1 ${-br}, ${br} 
+                        h ${-h + tr + br} 
+                        a ${bl}, ${bl} 0 0 1 ${-bl}, ${-bl} 
+                        v ${-h + tr + br}
+                        a ${tl}, ${tl} 0 0 1 ${tl}, ${-tl} 
+                        Z 
+                    `;
+                }
+                // Step 2-2: This is the inner path, drawing anti-clockwise
+                d += `
+                    M ${bw}, ${tl2 + bw}
+                    v ${h - tl2 - bl2 - bw * 2}
+                    a ${bl2}, ${bl2} 0 0 0 ${bl2}, ${bl2}
+                    h ${w - bl2 - br2 - bw * 2}
+                    a ${br2}, ${br2} 0 0 0 ${br2}, ${-br2} 
+                    v ${-h + tl2 + bl2 + bw * 2}
+                    a ${tr2}, ${tr2} 0 0 0 ${-tr2}, ${-tr2} 
+                    h ${-w + bl2 + br2 + bw * 2}
+                    a ${tl2}, ${tl2} 0 0 0 ${-tl2}, ${tl2} 
+                `;
+                // Step 3: Close the path
+                d += hollow ? `L ${tl2 + bw}, ${tl2 + bw}` : `z`;
+        }
     };
 
     clear = () => {
